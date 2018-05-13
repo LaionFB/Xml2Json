b7b1d9dff6155e827a564ef387577b65c3212a2a
Laion Fróis Biazão <laionfb@yahoo.com.br>
2018-05-13 17:24:30 -0300

    improved validation

--- src/br/edu/cefsa/ec6/laion/xml/XmlNodeArray.java
+++ src/br/edu/cefsa/ec6/laion/xml/XmlNodeArray.java
@@ -1,21 +1,17 @@
 package br.edu.cefsa.ec6.laion.xml;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 final class XmlNodeArray extends XmlNodeObject {
 	
 	public XmlNodeArray(String name, List<XmlNode> children){
 		super(name, children, '[', ']', false);
-		
-		List<String> childrenNames = this.children.stream().map(tag -> tag.getName()).collect(Collectors.toList());
-		for(int i = 1; i < childrenNames.size(); i++){
-			for(String tagName : childrenNames.subList(0, i)){
-				if(!tagName.equals(childrenNames.get(i))){
-					String format = "Tag '%s' is not an array since it's children has different names.";
-					throw new XmlParseException(String.format(format, name));
-				}
-			}
+
+		Stream<String> childrenNames = this.children.stream().map(tag -> tag.getName()).distinct();
+		if(childrenNames.count() > 1){
+			String format = "Tag '%s' is not an array since it's children has different names.";
+			throw new XmlParseException(String.format(format, name));
 		}
 	}
 	
